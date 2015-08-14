59b4c2fd61459bcdf1903a8b568986fbfccfafaa
Ivy <vyvymiao1217@outlook.com>
2015-08-13 23:49:10 -0400

    updates on 404 page and footer page, more styles as well

--- 404.php
+++ 404.php
@@ -1,13 +1,30 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<?php // Load Meta ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php  wp_title('|', true, 'right'); ?></title>
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<div class="main">
-  <div class="container">
+  <?php // Load our CSS ?>
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic|Playfair+Display:400,700|Raleway:400,200,300|Abril+Fatface' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
-    <h1>Not Found</h1>
-    <p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
-    <?php get_search_form(); ?>
+  <?php wp_head(); ?>
+</head>
 
+<section class="page_not_found">
+  <div class="page-wrapper">
+
+    <h2>404</h2>
+    <p>Page Not Found. <br>Looks like you're lost.</p>
+    <!-- <p></p> -->
+    <?php //get_search_form(); ?>
+	<a href=" <?php echo home_url() ?> ">Take Me Home</a>
   </div> <!-- /.container -->
-</div> <!-- /.main -->
-
-<?php get_footer(); ?>
+</section> <!-- /.main -->
+<?php wp_footer(); ?>
+</body>
+</html>
